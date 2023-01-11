926c93c0e0e00d8e66536bd21c698cdfc1be6bf6
lentio <lentio@hotmail.com>
2023-01-12 00:40:29 +0800

    support avi and filepath

--- check_photo.sh
+++ check_photo.sh
@@ -8,6 +8,7 @@ function lookupFile
     if test -f ${file}
     then
         ext="${file##*.}"
+        path="${file%/*}"
         if [ "${file##*.}"x = "JPG"x ] || [ "${file##*.}"x = "jpg"x ] || [ "${file##*.}"x = "jpeg"x ] || [ "${file##*.}"x = "HEIC"x ] #文件名满足的条件-正则表达式 *需要更改为您想匹配的条件，
         then
             subStr=`exiftool ${file} | grep "Date/Time Original" |  awk -F: 'NR==1{print $2"-"$3"-"$4"."$5"."$6}'` 
@@ -15,17 +16,17 @@ function lookupFile
             filename=`echo $subStr | sed -e 's/^[ \t]*//g'`
 
             # 文件名重定向
-            echo "rename ${file} to ${filename}.$ext"  
-            `mv "${file}" "${filename}.$ext"`
-        elif  [ "${file##*.}"x = "mp4"x ] || [ "${file##*.}"x = "MP4"x ] || [ "${file##*.}"x = "MOV"x ] || [ "${file##*.}"x = "mov"x ]
+            echo "rename ${file} to ${path}/${filename}.$ext"  
+            `mv "${file}" "${path}/${filename}.$ext"`
+        elif  [ "${file##*.}"x = "mp4"x ] || [ "${file##*.}"x = "MP4"x ] || [ "${file##*.}"x = "MOV"x ] || [ "${file##*.}"x = "mov"x ] || [ "${file##*.}"x = "AVI"x ] || [ "${file##*.}"x = "avi"x ]
         then
             subStr=`exiftool ${file} | grep "Media Create Date" |  awk -F: 'NR==1{print $2"-"$3"-"$4"."$5"."$6}'` 
             #获取文件名
             filename=`echo $subStr | sed -e 's/^[ \t]*//g'`
  
             # 文件名重定向
-            echo "rename ${file} to ${filename}.$ext" 
-            `mv "${file}" "${filename}.$ext"`
+            echo "rename ${file} to ${path}/${filename}.$ext" 
+            `mv "${file}" "${path}/${filename}.$ext"`
         fi
     elif test -d ${file} 
     then
@@ -38,7 +39,7 @@ function lookupFile
     echo "完成命令：$0 $file";
 }
 
-for file in `find . -name "*.mov" -o -name "*.MOV" -o -name "*.JPG" -o -name "*.jpg" -o -name "*.jpeg" -o -name ".MP4" -o -name "*.mp4" -o -name "*.HEIC"`
+for file in `find . -iname "*.heic" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.mov" -o -iname "*.mp4" -o -iname "*.avi"`
 do 
     echo "-----------------------------------------------"
     lookupFile ${file}
